0df04d776416f9121072a1336e84e52eda8cd7ed
Iago Bruno <ibruno.om@gmail.com>
2021-05-13 22:33:08 -0300

    Fix database config

--- src/app/models/index.js
+++ src/app/models/index.js
@@ -7,8 +7,13 @@ const basename = path.basename(__filename);
 const config = require('../../config/database');
 const db = {};
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+let sequelize;
 
+if (!process.env.DATABASE_URL) {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+} else {
+  sequelize = new Sequelize(process.env.DATABASE_URL, config);
+}
 
 fs
   .readdirSync(__dirname)
--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ require('dotenv').config({
 })
 
 module.exports = { 
-  url: process.env.DB_HOST,
+  host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
