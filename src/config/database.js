515778933e09d1d3eda16f32bcf990ec55ff7119
Iago Bruno <ibruno.om@gmail.com>
2021-05-13 23:15:46 -0300

    Fix database config

--- src/config/database.js
+++ src/config/database.js
@@ -7,7 +7,7 @@ module.exports = {
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  dialect: process.env.DB_DIALECT,
+  dialect: process.env.DB_DIALECT,    
   storage: './__tests__/database.sqlite',
   operatorsAliases: false,
   logging: false,
